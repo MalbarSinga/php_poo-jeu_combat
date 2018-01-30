2596fa73d4f2d43e2169bf05254ba1e60a714604
Singa <timalbar@gmail.com>
2018-01-30 22:59:54 +0100

    Update pdo.php
    connexion à la BDD avec un try catch qui renvoie une erreur ainsi que le code erreur en cas de soucis ^^
    après faudrait gerer les echo^-

--- lib/pdo.php
+++ lib/pdo.php
@@ -1,2 +1,8 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=combat;charset=utf8', 'root', '', [PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION]);
+try{
+    $bdd = new PDO('mysql:host=localhost;dbname=combat;charset=utf8', 'root', '', [PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION]);
+   // echo 'Vous etes bien connecte a la base de donnees';
+}
+catch(Exception $erreur_de_co){
+   echo 'VOUS avez un Soucis de '.' '.$erreur_de_co->getMessage().'Avec le code erreur :'.' '.$erreur_de_co->getCode();
+}
